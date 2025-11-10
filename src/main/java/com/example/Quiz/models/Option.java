b7c3a30ac0337f008292dba6b24442766ff04bb0
omarsalem22 <omarmohamed25112001@gmail.com>
2025-11-10 16:24:24 +0200

    add Option mode

---
+++ src/main/java/com/example/Quiz/models/Option.java
@@ -0,0 +1,27 @@
+package com.example.Quiz.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+    private String text;
+
+}
---
+++ src/main/java/com/example/Quiz/models/Question.java
@@ -0,0 +1,35 @@
+package com.example.Quiz.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne 
+    @JoinColumn(name = "quiz_id")
+    private  Quiz quiz;
+
+    private String text;
+
+    @OneToMany(mappedBy = "question",cascade=CascadeType.ALL,orphanRemoval=true)
+    private List<Option> options = new ArrayList<>();
+
+    private Integer correctOption;
+
+}
