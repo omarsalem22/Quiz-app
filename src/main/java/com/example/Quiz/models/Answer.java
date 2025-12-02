5c745defd8f2a7ddd8dd3c0a6d1a4cfea72cc15b
omarsalem22 <omarmohamed25112001@gmail.com>
2025-12-02 02:03:45 +0200

    add Answer model

---
+++ src/main/java/com/example/Quiz/models/Answer.java
@@ -0,0 +1,44 @@
+package com.example.Quiz.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "answers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Answer {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)  
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id",nullable=false)
+    private Quiz quiz;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    private String selectedOption;
+
+    private LocalDateTime timestamp;
+
+
+
+}
