0f559c81c6c10db2a9e6b9726a86508a62758d9d
omarsalem22 <omarmohamed25112001@gmail.com>
2025-12-02 02:15:47 +0200

    add score

---
+++ src/main/java/com/example/Quiz/models/Score.java
@@ -0,0 +1,37 @@
+package com.example.Quiz.models;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Score {
+
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz;
+
+    @Column(nullable = false)
+    private double scoreValue;
+
+    @CreationTimestamp
+    @Column(name = "completion_time", nullable = false, updatable = false)
+    private LocalDateTime completionTime;
+}
